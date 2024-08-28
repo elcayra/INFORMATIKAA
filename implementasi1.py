988af5d31ed5263ceee36f8ca537897aa2fcda6b
elcayra <145945728+elcayra@users.noreply.github.com>
2024-08-28 10:06:44 +0700

    Add files via upload

---
+++ implementasi1.py
@@ -0,0 +1,10 @@
+def introduce():
+    name = input("Elyca Nur Rahmadhani: ")
+    age = input("17: ")
+    boyfriend = input("Hong Joshua Seventeen: ")
+
+    print("\n--- Perkenalan Diri ---")
+    print(f"Halo! Nama saya {name}.")
+    print(f"Saya berusia {age} tahun.")
+    print(f"Saya memiliki kekasih bernama {boyfriend}.")
+    
