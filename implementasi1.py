6f9c3ff8b9a921824707a226e578f50c44f1645f
elcayra <145945728+elcayra@users.noreply.github.com>
2024-08-31 13:22:00 +0700

    Add files via upload

--- implementasi1.py
+++ implementasi1.py
@@ -1,10 +1,46 @@
-def introduce():
-    name = input("Elyca Nur Rahmadhani: ")
-    age = input("17: ")
-    boyfriend = input("Hong Joshua Seventeen: ")
+#Penulisan String
+print("Hello Elyca")
+print('''Ayo belajar python dari nol''')
 
-    print("\n--- Perkenalan Diri ---")
-    print(f"Halo! Nama saya {name}.")
-    print(f"Saya berusia {age} tahun.")
-    print(f"Saya memiliki kekasih bernama {boyfriend}.")
-    
+#Penulisan blok program
+if 5 > 2:
+    print("five is greater than two!")
+
+#Penulisan case
+my_variable_name ="Elyca " #snake case
+MyVariableName = "Rahmadhani" #pascal case
+
+print (my_variable_name + MyVariableName)
+
+
+#variable 1
+x = 9
+y = "Gasiwa"
+
+print(x)
+print(y)
+
+#variable 2
+N = "Elyca"
+T = "GAMADA"
+U = 2025
+print(N, T, U)
+
+#variable umur
+umur = input("masukan umur: ")
+if int(umur) < 18:
+    keterangan = 'masih muda'
+else:
+    keterangan = 'sudah tua'
+
+print('Elyca memasukan umur {}, artinya Elyca {}'.format(umur, keterangan))
+
+
+#komentar
+#Bisa menggunakan pagar
+'''Bisa menggunakan petik'''
+"""
+This is a comment
+written in
+more than just one line
+"""
